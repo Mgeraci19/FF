3943577b4d7f8b050852e303485e3b25b7804658
Mike <mickydof@gmail.com>
2024-10-31 14:28:04 -0400

    Minor website tweaks

---
+++ app/pages/Homepage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Homepage.css'
+
+export function Homepage() {
+    return(
+        <div>
+            <h3>Welcome to Mitch Millers Nightmare!!!</h3>
+
+
+        </div>
+    )
+}
